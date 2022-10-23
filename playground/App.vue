9a54fe79217306994ba403e844c4f0c8f14cbc5c
Thomas Cazade <cazade.thomas@gmail.com>
2022-10-23 09:45:29 +0200

    Add v-if and media-query on sandbox slider

--- playground/App.vue
+++ playground/App.vue
@@ -1,29 +1,47 @@
 <template>
-  <BlazeSlider
-    :options="{
-      all: {
-        slidesToShow: 3,
-        slideGap: '2rem',
-      },
-    }"
-  >
-    <template #default>
-      <BlazeSlide>1</BlazeSlide>
-      <BlazeSlide>2</BlazeSlide>
-      <BlazeSlide>3</BlazeSlide>
-      <BlazeSlide>4</BlazeSlide>
-    </template>
+  <div class="app-container">
+    <BlazeSlider
+      v-if="showSlider"
+      :options="{
+        all: {
+          slidesToShow: 3,
+          slideGap: '2rem',
+        },
+        '(max-width: 425px)': {
+          slidesToShow: 1,
+        },
+      }"
+    >
+      <template #default>
+        <BlazeSlide>1</BlazeSlide>
+        <BlazeSlide>2</BlazeSlide>
+        <BlazeSlide>3</BlazeSlide>
+        <BlazeSlide>4</BlazeSlide>
+      </template>
 
-    <template #pagination>
-      <BlazePagination />
-    </template>
-  </BlazeSlider>
+      <template #pagination>
+        <BlazePagination />
+      </template>
+    </BlazeSlider>
+
+    <button
+      type="button"
+      class="toggle-button"
+      @click="showSlider = !showSlider"
+    >
+      Hide/show slider
+    </button>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
+
 import BlazeSlider from "../src/BlazeSlider.vue";
 import BlazeSlide from "../src/BlazeSlide.vue";
 import BlazePagination from "../src/BlazePagination.vue";
+
+const showSlider = ref(true);
 </script>
 
 <style lang="css">
@@ -55,6 +73,27 @@ body {
 </style>
 
 <style lang="css" scoped>
+.app-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.toggle-button {
+  display: inline-block;
+  margin-top: 2rem;
+  padding: 0.5rem 1.25rem;
+  font-size: 1rem;
+  border-radius: 0.25rem;
+  color: #f8fafc;
+  background-color: #334155;
+  cursor: pointer;
+}
+
+:deep(.blaze-slider) {
+  width: 100%;
+}
+
 :deep(.blaze-slide) {
   display: flex;
   align-items: center;
